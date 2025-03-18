6caba48d50eec33a062d734fde9d15947c10d89f
Jose Carrizales <josedcarrizales@gmail.com>
2025-03-17 20:19:48 -0700

    Initial Commit

---
+++ Test App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Test App
+//
+//  Created by Jose Carrizales on 3/17/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Test App/Test_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_AppApp.swift
+//  Test App
+//
+//  Created by Jose Carrizales on 3/17/25.
+//
+
+import SwiftUI
+
+@main
+struct Test_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

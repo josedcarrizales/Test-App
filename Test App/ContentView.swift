58372e8d4216fcd08e02a10c59bd6b5bbb01361b
Jose Carrizales <josedcarrizales@gmail.com>
2025-03-23 00:07:16 -0700

    Created a horizontally scrollable card

--- Test App/ContentView.swift
+++ Test App/ContentView.swift
@@ -9,14 +9,53 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Image("cream-bg")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            ScrollView(.horizontal) {
+                HStack {
+                    ForEach(0..<20) { _ in
+                        cardStyle
+                }
+            }
         }
+    }
+}
+
+var cardStyle: some View {
+    VStack {
+        ZStack {
+                locationImage
+                locationInfo
+            }
+        }
+        .frame(width: 250, height: 250)
+        .shadow(radius: 5, x: 5, y: 5)
         .padding()
     }
+}
+
+var locationImage: some View {
+    Image("laguna")
+        .resizable()
+        .shadow(radius: 5, x: 5, y: 5)
+}
+
+var locationInfo: some View {
+    HStack {
+        VStack(alignment: .leading) {
+            Text("Laguna")
+                .font(.headline)
+            Text("Laguna Beach, CA")
+                .font(.caption)
+        }
+        Spacer()
+        Image(systemName: "heart")
+    }
+    .padding()
+    .background(Color.gray.opacity(0.2).gradient)
+    .foregroundStyle(.white)
 }
 
 #Preview {

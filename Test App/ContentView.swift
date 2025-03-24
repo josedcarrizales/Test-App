5f0b8d35f819ab076ca53b70fbdb06bf3f8fd0a0
Jose Carrizales <josedcarrizales@gmail.com>
2025-03-23 22:31:23 -0700

    Updated code to read from the beaches array containing beach classes instead of static data

--- Test App/ContentView.swift
+++ Test App/ContentView.swift
@@ -14,49 +14,58 @@ struct ContentView: View {
                 .resizable()
                 .edgesIgnoringSafeArea(.all)
             ScrollView(.horizontal) {
-                HStack {
-                    ForEach(0..<20) { _ in
-                        cardStyle
-                }
+                    locationInfo
             }
         }
     }
-}
-
-var cardStyle: some View {
-    VStack {
-        ZStack {
-                locationImage
-                locationInfo
-            }
-        }
-        .frame(width: 250, height: 250)
-        .shadow(radius: 5, x: 5, y: 5)
-        .padding()
-    }
-}
-
-var locationImage: some View {
-    Image("laguna")
-        .resizable()
-        .shadow(radius: 5, x: 5, y: 5)
 }
 
 var locationInfo: some View {
     HStack {
-        VStack(alignment: .leading) {
-            Text("Laguna")
-                .font(.headline)
-            Text("Laguna Beach, CA")
-                .font(.caption)
+        ForEach(beaches.indices) { index in
+                ZStack {
+                    Image(beaches[index].image)
+                        .resizable()
+                        .shadow(radius: 5, x: 5, y: 5)
+                    HStack {
+                        VStack {
+                            Text(beaches[index].name)
+                                .font(.headline.bold())
+                            Text(beaches[index].location)
+                                .font(.subheadline)
+                        }
+                        .padding()
+                        Spacer()
+                        Image(systemName: "heart")
+                            .padding()
+                    }
+                    .background(Color.gray.opacity(0.2))
+                    .position(x: 150, y: 35)
+                }
+                .frame(width: 300, height: 300)
+                .padding()
         }
-        Spacer()
-        Image(systemName: "heart")
     }
-    .padding()
-    .background(Color.gray.opacity(0.2).gradient)
     .foregroundStyle(.white)
 }
+
+class Beach {
+    var name: String
+    var location: String
+    var image: String
+    
+    init(name: String, location: String, image: String) {
+        self.name = name
+        self.location = location
+        self.image = image
+    }
+}
+
+var laguna: Beach = Beach(name: "Laguna", location: "Laguna Beach, CA", image: "laguna")
+
+var newport: Beach = Beach(name: "Newport", location: "Newport Beach, CA", image: "laguna")
+
+var beaches = [laguna,newport]
 
 #Preview {
     ContentView()
